454aeed6e6733e6a6838630cf757eea750aa27a5
Antony Nyagah <antonynyagah@Antonys-MacBook-Pro.local>
2020-12-27 15:04:55 -0600

    added airship web push notifications

---
+++ public/push-worker.js
@@ -0,0 +1,10 @@
+// 86acbd31cd7c09cf30acb66d2fbedc91daa48b86:1609102606.5058606
+importScripts('https://aswpsdkus.com/notify/v1/ua-sdk.min.js')
+uaSetup.worker(self, {
+  defaultIcon: 'https://tony\u002Dnyagah\u002Dportfolio.herokuapp.com/static/media/IMG_7710.3f5e8b09.png',
+  defaultTitle: 'Antony\u0027s Portfolio',
+  defaultActionURL: 'https://tony\u002Dnyagah\u002Dportfolio.herokuapp.com/',
+  appKey: 'wJZEvSytR6aFjbH5xAfpXw',
+  token: 'MTp3SlpFdlN5dFI2YUZqYkg1eEFmcFh3Ono4dkFJTXBCbkd0Y3JRU3VkWnpkQmNyZThMVEZXVUo0dEl6RlJIbFdjMVE',
+  vapidPublicKey: 'BKN--767Ys-oO5BoUNYWGexidT79lyOmD-BTd06b_UjB6M6hFe9GApxY9Je06ThB5dJOON7L5M133uub1GLPQlI='
+})
